52d6f08ccd894aa60615373ef7497d8e1f045739
vijay22uk <vpandey@merrillcorp.com>
2016-10-25 12:34:24 +0530

    hashtag search

--- server/routes/domain.js
+++ server/routes/domain.js
@@ -1,26 +1,26 @@
 var request = require('request');
 var util = require('util');
-var swKey = process.env.KEY;  
+var swKey = process.env.KEY;
 var baseUrl = "http://api.similarweb.com/Site/";
 
 module.exports = function (app) {
     app.route('/api/competitor')
         .get(function (req, res) {
-            var siteName = req.param('siteName')
+            var siteName = '#' + req.param('siteName')
             var subUrl = util.format(baseUrl + "%s/v1/%s?UserKey=%s&Format=JSON", siteName.toLowerCase(), "similarsites", swKey);
-            console.log("URL IS :: %s ",subUrl);
+            console.log("URL IS :: %s ", subUrl);
             request(subUrl, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     res.status(200).send(body);
-                }else{
+                } else {
                     console.log(error);
-                    res.status(500).send("Url kya tha bhai");
+                    res.status(500).send("Url not valid");
                 }
             })
 
         })
         .post(function (req, res) {
             var _book = req.body;
-             res.status(200).send();
+            res.status(200).send();
         })
 }
